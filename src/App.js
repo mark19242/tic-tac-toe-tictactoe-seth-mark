c349ee00ae50aa04ae6b377f5d99ae2a4ffcb9cc
mark19242 <mark19242@gmail.com>
2024-02-29 11:24:58 -0800

    getting the winner

--- src/App.js
+++ src/App.js
@@ -4,37 +4,73 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
-
-  const [playerMove, setPlayerMove] = useState("x")
+   const [playerMove, setPlayerMove] = useState("x")
+   const [winner, setWinner] = useState(null)
 
  
+   const calculateWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+  
+    
+    const checkWinner = () => {
+      for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+         setWinner(squares[a])
+      }
+     
+    }
+    
 
-  const moveStatus = () => {
+    return null;
+  }
+  
+checkWinner()
+   }
+ 
+  
+  
+  
+  
+   const moveStatus = () => {
     setPlayerMove(playerMove === "x" ? "o" : "x")
   }
-
 
   const handleGamePlay = (currentSquare) => {
     if(squares[currentSquare]=== null ) {
        squares[currentSquare] = playerMove
        moveStatus()
-         
-   
+       setSquares([...squares])
+       calculateWinner()
+       
+    
+      // } else {
+      //   squares[currentSquare] === !calculateWinner
+      //   return "cats game"
       }
     }
-
+console.log(handleGamePlay)
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <div className='board'>
-        {squares.map((squares, index) => {
-          console.log(index);
+        {squares.map((square, index) => {
+          // console.log(square);
 
 
           return (
             <Square 
-            square={squares}
+            square={square}
             key={index}
             index={index}
             handleGamePlay={handleGamePlay}
@@ -49,5 +85,4 @@ const App = () => {
     </>
   )
 }
-
 export default App
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 const Square = (props) => {
-
+  
   const handleClick = () => {
     props.handleGamePlay(props.index)
-}
-
+  }
   return (
-    <div className="square" onClick={handleClick}>
-        {props.square}
+     <div className="square" onClick={handleClick}>
+      {props.square}
     </div>
   )
 }
-
 export default Square
