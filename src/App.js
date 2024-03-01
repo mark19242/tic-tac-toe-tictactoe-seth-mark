1e206178ae60c1c8329c471d38f74c16bf05e28a
sethcrist <sethcrist50@gmail.com>
2024-02-29 16:18:28 -0800

    finished exercise

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react'
 import Square from './components/Square'
 import './App.css'
 
+
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
-   const [playerMove, setPlayerMove] = useState("x")
+   const [playerMove, setPlayerMove] = useState("X")
    const [winner, setWinner] = useState(null)
 
- 
+
+   const restartGame = () => {
+    window.location.reload()
+  }
+
+// code for checking if theres a winner/ tie game
    const calculateWinner = () => {
     const lines = [
       [0, 1, 2],
@@ -19,30 +25,42 @@ const App = () => {
       [0, 4, 8],
       [2, 4, 6],
     ]
-  
-    
+
     const checkWinner = () => {
+      let tieGame = true
       for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-         setWinner(squares[a])
-      }
-     
-    }
-    
+        setWinner(squares[a])
+        setTimeout(() => {
+          alert("You Win!!!")
+        window.location.href = 'https://youtube.com/shorts/KabwIEv37h4?si=P1yoKpnE4LronQXn'
 
-    return null;
+        }, "1000");
+        return
+      } 
+      if (squares[a] === null || squares[b] === null || squares[c] === null) { //if it finds an empty square, it can't be a tie
+        tieGame = false
+      }
+    }
+    if (tieGame) {
+      setWinner("tie")
+      setTimeout(() => {
+        alert("Cats Game")
+        window.location.href = 'https://youtube.com/shorts/KabwIEv37h4?si=P1yoKpnE4LronQXn'
+      }, "1000");
+    }
   }
-  
-checkWinner()
+    checkWinner()
    }
+  
  
   
   
   
-  
+// code for player 1(X) / player 2(O)
    const moveStatus = () => {
-    setPlayerMove(playerMove === "x" ? "o" : "x")
+    setPlayerMove(playerMove === "X" ? "O" : "X")
   }
 
   const handleGamePlay = (currentSquare) => {
@@ -51,37 +69,28 @@ checkWinner()
        moveStatus()
        setSquares([...squares])
        calculateWinner()
-       
-    
-      // } else {
-      //   squares[currentSquare] === !calculateWinner
-      //   return "cats game"
       }
     }
-console.log(handleGamePlay)
+
+    
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      <p>Current Player: {playerMove}</p>
       <div className='board'>
         {squares.map((square, index) => {
-          // console.log(square);
-
-
           return (
             <Square 
             square={square}
             key={index}
             index={index}
             handleGamePlay={handleGamePlay}
-            // moveStatus={moveStatus}
-            //playerMove={playerMove}
-            
             />
-
           )
         })}
       </div>
+        <button className='btn' type="button" onClick={restartGame}>Play Again</button>
     </>
   )
 }
